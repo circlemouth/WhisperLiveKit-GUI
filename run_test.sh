330e1155034b864a80e8cccb81e3bfddfb762f7b
Silas Kieser <silas.kieser@gmail.com>
2025-01-20 10:22:26 +0100

    update parameters

--- run_test.sh
+++ run_test.sh
@@ -5,10 +5,10 @@ python translation_online.py \
 --task transcribe \
 --backend mlx-whisper \
 --buffer_trimming sentence \
---min-chunk-size 3\
- --buffer_trimming_sec 5 \
+--min-chunk-size 2 \
 --audio_path preche.wav \
 --vac \
---vac-chunk-size 2 \
 $argv
 
+#--vac-chunk-size 2 \
+# --buffer_trimming_sec 5 \
