52d48f7ab09c964ed314bb7abf6d46ce14e0df27
Silas Kieser <silas.kieser@gmail.com>
2025-02-05 14:29:32 +0100

    timestamped words with concatenate

--- src/whisper/timestaped_words.py
+++ src/whisper/timestaped_words.py
@@ -1,5 +1,8 @@
 from typing import List
 
+import logging
+logger = logging.getLogger(__name__)
+
 class TimeStampedSegment:
     """
     Represents a segment of text with start and end timestamps.
@@ -20,7 +23,7 @@ class TimeStampedSegment:
     def __repr__(self):
         return self.__str__()
     
-    def shift(self, shift: float):
+    def shift(self, shift: float) -> None:
         """
         Shifts the segment by a given amount of time.
 
@@ -35,7 +38,8 @@ class TimeStampedSegment:
             >>> segment.shift(1.0)
             1.0 - 2.0: Hello
         """
-        return TimeStampedSegment(self.start + shift, self.end + shift, self.text)
+        self.start += shift
+        self.end += shift
     
     def append_text(self, text: str):
         """
@@ -63,45 +67,122 @@ class TimeStampedSegment:
         else:
             raise TypeError(f"unsupported operand type(s) for +: '{type(self).__name__}' and '{type(other).__name__}'")
 
-class TimeStampedText:
+class TimeStampedSequence(list):
     """
     Represents a collection of TimeStampedSegment instances.
-
-    Attributes:
-        segments (List[TimeStampedSegment]): The list of segments.
     """
-    def __init__(self):
-        self.segments: List[TimeStampedSegment] = []
+    def __init__(self, segments: List[TimeStampedSegment] = None,sep=None):
+        segments = segments if segments is not None else []
+        super().__init__(segments)
+        
+        if sep is None:
+            self.sep = self.infer_sep()
 
-    def add_segment(self, segment: TimeStampedSegment):
+
+    def infer_sep(self) -> None:
+        if len(self) <= 3:
+            
+            self.sep = None
+        else:
+            n_tailing_spaces = 0
+            for word in self:
+                if word.text.endswith(" "):
+                    n_tailing_spaces += 1
+
+
+            if n_tailing_spaces / len(self) > 0.5:
+                sep=""
+            else:
+                sep= " "
+
+            logger.debug(f"{n_tailing_spaces} words of {len(self)} have tailing spaces I will use '{sep}' to join them")
+            
+            self.sep = sep
+
+    def concatenate(self) -> TimeStampedSegment:
         """
-        Adds a segment to the collection.
+        Concatenates all segments in the sequence into a single segment.
 
-        Args:
-            segment (TimeStampedSegment): The segment to add.
+        Returns:
+            TimeStampedSegment: A single segment containing all text from the sequence.
 
         Example:
-            >>> tst = TimeStampedText()
-            >>> tst.add_segment(TimeStampedSegment(0.0, 1.0, "Hello"))
-            >>> tst.add_segment(TimeStampedSegment(1.0, 2.0, "world"))
-            >>> len(tst)
-            2
+            >>> sequence = TimeStampedSequence([TimeStampedSegment(0.0, 1.0, "Hello"), TimeStampedSegment(1.0, 2.0, "World")])
+            >>> sequence.concatenate()
+            0.0 - 2.0: HelloWorld
         """
-        self.segments.append(segment)
+        if len(self) == 0:
+            return TimeStampedSegment()
+        
+        sep = " " if self.sep is None else self.sep
 
-    def __repr__(self):
-        return f"TimeStampedText(segments={self.segments})"
-
-    def __iter__(self):
-        return iter(self.segments)
-
-    def __getitem__(self, index):
-        return self.segments[index]
-
-    def __len__(self):
-        return len(self.segments)
+        return TimeStampedSegment(self[0].start, self[-1].end, sep.join(segment.text for segment in self))
     
-    # TODO: a function from_whisper_res()
+    def get_text(self,sep=None) -> str:
+        """
+        Returns the text of all segments in the sequence.
+
+        Returns:
+            str: The text of all segments in the sequence.
+
+        Example:
+            >>> sequence = TimeStampedSequence([TimeStampedSegment(0.0, 1.0, "Hello"), TimeStampedSegment(1.0, 2.0, "World")])
+            >>> sequence.get_text(sep=' ')
+            'Hello World'
+        """
+        if sep is None:
+            if self.sep is None:
+                sep = " "
+            else:
+                sep = self.sep
+
+        
+        return sep.join(segment.text for segment in self)
+    
+    def shift(self, shift: float) -> None:
+        """
+        Shifts all segments in the sequence by a given amount of time.
+
+        Args:
+            shift (float): The amount of time to shift the segments.
+
+        Returns:
+            TimeStampedSequence: A new sequence with all segments shifted by the given amount of time.
+
+        Example:
+            >>> sequence = TimeStampedSequence([TimeStampedSegment(0.0, 1.0, "Hello"), TimeStampedSegment(1.0, 2.0, "World")])
+            >>> sequence.shift(1.0)
+            [1.0 - 2.0: Hello, 2.0 - 3.0: World]
+        """
+        for segment in self:
+            segment.shift(shift)
+    
+    def __repr__(self):
+
+        if len(self) == 0:
+            return "TimeStampedSequence([])"
+
+        representation = f"TimeStampedSequence({n} words from {start} to {end}: {first}…{last})".format(
+            n= len(self),
+            start= self[0].start,
+            end= self[-1].end,
+            first= self[0].text,
+            last= self[-1].text
+        )
+
+        return representation
+    
+    def __str__(self):
+        
+        if len(self) == 0:
+            return "TimeStampedSequence([])"
+
+        return self.concatenate().__str__()
+
+
+
+
+
 
 if __name__ == "__main__":
     import doctest
